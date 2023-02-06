e616a9675535c127e428299fc9c94f9e3d45b774
Danilo Pereira <dcandido@recurly.com>
2023-02-06 14:34:10 -0300

    Add password to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+
   has_one_attached :avatar_image
+  enum role: { no_admin: 0, admin: 1 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_06_162408) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_06_170450) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_06_162408) do
     t.integer "role", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "unique_emails", unique: true
   end
 
