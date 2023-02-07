63f308d47abbbcb536c405fa39d80188a84649e1
Danilo Pereira <dcandido@recurly.com>
2023-02-07 10:20:29 -0300

    List users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
   load_and_authorize_resource
 
   def index
+    # TODO: refatorar
+    @users = User.all
+    @total = @users.size
+    @total_by_role = @users.group(:role).count
   end
 
   def new
@@ -16,11 +20,18 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      session[:user_id] = user.id
-      redirect_to '/'
+      session[:user_id] = user.id unless current_user
+      redirect_to root_path
     else
       redirect_to '/signup'
     end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    flash[:success] = 'The user was successfully destroyed.'
+    redirect_to root_path
   end
 
   private
